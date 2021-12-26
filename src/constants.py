019493e41f913b5adfc177b5e88066aaca724e42
ovidiu99 <gherghinaovidiu1999@gmail.com>
2021-12-26 17:28:22 +0200

    Added the second page

--- src/constants.py
+++ src/constants.py
@@ -1,2 +1,8 @@
 # COLOURS
 BACKGROUND_COLOUR = "#fff5cc"
+PROGRESS_BAR_BORDER_COLOUR = "#997a00"
+
+# FONTS
+TITLE_FONT = ("Helvetica", 30, "bold")
+LABEL_FONT = ("Helvetica", 24)
+LABEL_FONT_BOLD = ("Helvetica", 24, "bold")
--- src/user.py
+++ src/user.py
@@ -1,9 +1,13 @@
 class User:
-    number_of_iterations_calm_state = 1
-    average_eeg_calm_state = [0, 0, 0, 0]
-    average_eeg_jaw_clench = [0, 0, 0, 0]
+    def __init__(self):
+        self.number_of_iterations_calm_state = 1
+        self.average_eeg_calm_state = [0, 0, 0, 0]
+        self.average_eeg_jaw_clench = [0, 0, 0, 0]
 
     def update_average_eeg_calm_state(self, eeg_list):
         self.average_eeg_calm_state = [
-            self.average_eeg_calm_state[i] + eeg_list[i] for i in range(len(eeg_list))
+            (i * (self.number_of_iterations_calm_state - 1) + j)
+            / self.number_of_iterations_calm_state
+            for i, j in zip(self.average_eeg_calm_state, eeg_list)
         ]
+        self.number_of_iterations_calm_state += 1
